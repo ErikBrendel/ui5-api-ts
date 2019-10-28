15166a98bd2782bb4e61d1573f1b0a715d27cb07
Erik Brendel <erik.brendel@sap.com>
2019-10-28 15:43:10 +0100

    speed up downloading api

--- scripts/download.py
+++ scripts/download.py
@@ -9,11 +9,17 @@ URL_START = "https://sapui5.hana.ondemand.com/test-resources/"
 URL_END = "/designtime/apiref/api.json"
 
 
+handled = []
+
+
 def url_for_module(name: str) -> str:
     return URL_START + name.replace(".", "/") + URL_END
 
 
 def dl(url: str, file_name: str) -> dict:
+    if file_name in handled:
+        return {}
+    handled.append(file_name)
     req = requests.get(url)
 
     # 2. Handle error if deserialization fails (because of no text or bad format)
@@ -29,7 +35,7 @@ def dl(url: str, file_name: str) -> dict:
 
 
 def load_main_node(symbol: dict):
-    name = symbol['name']
+    name = symbol['lib']
     dl(url_for_module(name), name)
     if "nodes" in symbol:
         for sub_node in symbol["nodes"]:
